9d6b7c56cac2aa896183ab19d2cf5aad39a36830
Vishal <vishaljankar2534@gmail.com>
2026-06-11 15:28:43 +0530

    desiigning login page done

--- frontend/react/src/login.jsx
+++ frontend/react/src/login.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import { useState } from 'react'
+import './index.css'
 
 const Login = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    
   return (
-    <div className='login'>
+    <div className='signup-form'>
         <h1>Login here</h1>
         <label >
             Username: <input />
         </label>
-        Password: <input />
+        <label >
+            Password: <input />
+        </label>
+        <button className='submit' type='submit' >submit</button>
     </div>
+    
   )
 }
 
